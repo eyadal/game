658f0327b25672ae5d4997127bf4bb191ca1afca
eyadal <31968847+eyadal@users.noreply.github.com>
2020-12-10 01:34:10 +0100

    userInput function created

--- assets/index.js
+++ assets/index.js
@@ -1,7 +1,17 @@
+// Selectors
 let gameBoard = document.querySelector('.game-board')
+
+// Global Variables
+let guessedLetter
+
+// Arrays
 let fruits = ['Apple', 'Passion', 'Pineapple', 'Sharon', 'Orange']
 let fruits2 = ['Apple2', 'Passion2', 'Pineapple2', 'Sharon2', 'Orange2']
-// console.log(fruits[1])
+
+// Event Handlers
+window.addEventListener('keyup', userInput)
+
+// Functions
 function startGame(arr) {
 
     let secretWord = random(arr)
@@ -11,30 +21,35 @@ function startGame(arr) {
 }
 
 function random(myArr){
+    console.log('myArr: ' + myArr)
     let randomPick = Math.floor(Math.random() * Math.floor(myArr.length) ) // ger dig ett indexvärde
+    console.log('randomPick: ' + randomPick)
+    console.log('return: '+myArr[randomPick])
     return myArr[randomPick] // ger dig ordet beroende på indexvärdet
 }
 
 
 
 function spellArray(word){
-    return word.split('') // vårat ord blir till en array med bokstäver
+    return word.split('') // vårat ord blir till en array med bokstäver ['O','r','a','n','g','e']
 }
 
 
 
 function hiddenWord(letters) {
-    gameBoard.append(letters.map(  letter => letter = '*' ).join(' '))
+    gameBoard.append( letters.map(  letter => letter = '*' ).join(' '))
+        guessLetter(guessedLetter)
 }
 
 
 
 
 
-
-function matchUserData() {
-    
+function userInput(e) {
+    guessedLetter = e.key
+   return guessedLetter
 } 
+
 // Topping
 function clearGameboard() {
     gameBoard.innerHTML = ''
